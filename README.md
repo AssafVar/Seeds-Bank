4028b8fc1e23363a2d55ed47fc2085dc5ca014cf
assaf21 <assaf@ad-knight.com>
2023-01-03 09:29:42 +0200

    React and NodeJS backbone

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Seeds-Bank
+
+This app tend to serve breeders that want to screen, document and develop new breeds.
---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Seeds Bank</h1>
+    </div>
+  );
+}
+
+export default App;
