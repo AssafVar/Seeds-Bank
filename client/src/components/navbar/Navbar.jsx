5b7487fbd9ee3e139b1204757b73b153c76fdaf3
assaf21 <assaf@ad-knight.com>
2023-01-08 13:13:53 +0200

    frontend- navbar login design

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -2,7 +2,9 @@ import { Button, Tooltip, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import LoginModal from '../modals/RegisterModal';
 import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import useAuth from '../../hooks/useAuth';
 import "./navbar.css";
 
 
@@ -10,6 +12,7 @@ function Navbar(props) {
 
     const [isOpenModal, setIsOpenModal] = useState(false);
     const [isSignup, setIsSignup] = useState(true);
+    const {onLogout,activeUser} = useAuth();
 
     const handleLogin = () => {
         setIsSignup(false);
@@ -34,12 +37,18 @@ function Navbar(props) {
                 </div>
                 <div className='nav-right'>
                     <Button>My Profile</Button>
+                    {!activeUser&&<>
                     <Tooltip title="Login">
-                        <Button onClick={()=>{handleLogin();setIsSignup(false)}}><LoginIcon/></Button>
+                        <Button color='success' onClick={()=>{handleLogin();setIsSignup(false)}}><LoginIcon/></Button>
                     </Tooltip>
                     <Tooltip title="Signup">
-                        <Button onClick={()=>{handleLogin();setIsSignup(true)}}><PersonAddIcon/></Button>
+                        <Button color='success' onClick={()=>{handleLogin();setIsSignup(true)}}><PersonAddIcon/></Button>
                     </Tooltip>
+                    </>}
+                    {activeUser&&<>
+                    <Tooltip title="Logout">
+                        <Button color='error' onClick={onLogout} ><LogoutIcon/></Button>
+                    </Tooltip></>}
                 </div>
             </div>
         </>
