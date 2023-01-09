ff06cde8b38f28d3a068309a1338bd3055b74742
assaf21 <assaf.vardy@gmail.com>
2023-01-09 10:35:31 +0200

    frontend- profile page primery css

--- client/src/pages/profile/ProfilePage.jsx
+++ client/src/pages/profile/ProfilePage.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Button } from '@mui/material';
+import './profilePage.css';
 
 function ProfilePage(props) {
+
     return (
-        <div>
-          <h1>Profile Page</h1>  
+        <div className='my-profile'>
+            <div className="profile-sidebar">
+                <Button>Personal Info</Button>
+                <h1>My Websites</h1>
+                <ul>
+                    <li>web</li>
+                    <li>site</li>
+                    <li>link</li>
+                    <li>other link</li>
+                </ul>
+            </div>
+            <div className="profile-main-container">
+                <div className="profile-my-projects">
+                    my-open-projects
+                </div>
+
+            </div>
         </div>
     );
 }
