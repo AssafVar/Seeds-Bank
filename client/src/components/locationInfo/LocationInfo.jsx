c17c2a50f93f233bbaa7a01a4fef2fef47d97c42
assaf21 <assaf.vardy@gmail.com>
2023-01-23 22:55:19 +0200

    frontend- self location

--- client/src/components/locationInfo/LocationInfo.jsx
+++ client/src/components/locationInfo/LocationInfo.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 import "./locationInfo.css";
 
 function LocationInfo(props) {
+
+    const [position, setPosition] = useState('');
+    
+    useEffect(()=>{
+        navigator.geolocation.getCurrentPosition((position)=>setPosition(position.coords), (error)=>console.log(error))
+    },[]);
+
     return (
         <div className='location-info'>
-            <span>Location</span>
+            <span>Location  lat:{position.latitude}, lng:{position.longitude}</span>
             <span>Temp</span>
             <span>Clowdy</span>
         </div>
