d7e3c526b1e2d476134c81025dd13d56c48c15a8
assaf21 <assaf.vardy@gmail.com>
2023-03-05 12:58:23 +0200

    frontend - about page

--- client/src/pages/about/AboutPage.jsx
+++ client/src/pages/about/AboutPage.jsx
@@ -1,11 +1,97 @@
-import React from 'react';
+import { Button, Card, CardMedia, Grid, Typography } from "@mui/material";
+import { Box, color, Container } from "@mui/system";
+import React, { useState } from "react";
+
+const imageUrl =
+  "https://c.pxhere.com/photos/a6/1e/large_broad_leaf_plant_leaves_lush_colorful_green_nature_foliage_plant_large_leaves_green_clustered-969259.jpg!d";
 
 function AboutPage(props) {
-    return (
-        <div>
-            About
-        </div>
-    );
+  const [readMore, setReadMore] = useState(false);
+
+  return (
+    <Container>
+      <Box
+        sx={{ backgroundImage: `url(${imageUrl})`, backgroundSize: "cover" }}
+      >
+        <Typography variant="h3" padding={5} margin={"3rem 0"} color={"white"}>
+          About Us
+        </Typography>
+      </Box>
+      <Grid container spacing={3}>
+        <Grid item xs={12} md={6}>
+          <Typography variant="body1">
+            Our application is a software tool that helps plant breeders manage
+            and organize their breeding programs.
+          </Typography>
+          <Typography variant="body1">
+            With the aid of SeedsBank, breeders can maintain a comprehensive
+            database of their plants, including their pedigree, genotype, and
+            phenotype information. They can track each plant's lineage and use
+            this information to determine the optimal breeding pairs to produce
+            desired traits in their offspring.
+          </Typography>
+          <br />
+          {readMore && (
+            <>
+              <Typography variant="body1">
+                {" "}
+                SeedsBank application also includes tools for managing plant
+                health records, such as disease resistance, susceptibility, and
+                environmental tolerance. This information can be used to ensure
+                that the breeding program produces healthy plants with desirable
+                traits.
+              </Typography>
+              <Typography variant="body1">
+                {" "}
+                In addition, it provides features for tracking plant performance
+                metrics, such as yield, quality, and growth rate. This
+                information can help breeders make informed decisions about
+                which plants to keep for breeding and which ones to discard.
+              </Typography>
+              <br />
+              <Typography variant="body1">
+                {" "}
+                One of the main benefits of a plant breeding application is that
+                it can help breeders streamline their breeding program, by
+                allowing them to quickly and easily identify plants with
+                desirable traits and select optimal breeding pairs.
+              </Typography>
+              <Typography variant="body1">
+                {" "}
+                This can help save time, reduce costs, and increase the
+                likelihood of producing high-quality plants with desirable
+                traits.
+              </Typography>
+              <Typography variant="body1">
+                <br /> Overall, SeedsBank application can be a powerful tool for
+                plant breeders, helping them to manage and optimize their
+                breeding programs, and ultimately produce better, more
+                resilient, and more productive plants.
+              </Typography>
+              <Typography variant="body1">
+                {" "}
+                The purpose of a plant breeding application is to enable
+                breeders to keep track of their plants' genetic traits, monitor
+                their health and performance, and make informed decisions about
+                breeding.
+              </Typography><br/>
+            </>
+          )}
+          <Button onClick={() => setReadMore(!readMore)} style={{borderRadius:"5px", }} variant="contained" color="success">
+            {!readMore ? "More Info" : "Less Info"}
+          </Button>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Card>
+            <CardMedia
+              image="https://pxhere.com/en/photo/1130736?utm_content=shareClip&utm_medium=referral&utm_source=pxhere"
+              title="About Us Image"
+            />
+          </Card>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
 
 export default AboutPage;
